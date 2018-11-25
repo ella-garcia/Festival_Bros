0b446486d0f083deb50de78a5a0eb4692aaef146
Clarice <clarice_norton@utexas.edu>
2018-11-25 17:14:13 -0600

    Refactored a tiny bit -Clarice

--- festival_scheduler.py
+++ festival_scheduler.py
@@ -4,12 +4,12 @@ user_locations = ["Austin", "New York", "Dallas", "San Francisco", "Santa Fe", "
 user_times = [1, 13.5, 14, 22, 19.25, 20.5, 13.5, 27, 0, 24, -1.5, 22] #has 27, 0, 24, -1 to test that the times are greater than or equal to 0 and less than 24, and has duplicates to test conflicting times
 
 
-#Function to select the location that the user wants and output the festivals in that location
+#Function to select the location that the user wants and output the festivals in that location or a function that checks that all the locations of the festivals that were given to us are in the same location if that's what we're going for?
 def select_location(user_location):
 
 
 
-#Function to select the genre the user wants and output the festivals with that genre in the previously selected city 
+#Function to take the genre the user wants and output the festivals with that genre in the previously selected city 
 def select_genre(user_genre):
 
 
