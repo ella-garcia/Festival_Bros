7ba3f3dfafe119abf27a775c798505e1047ab9a4
Clarice <clarice_norton@utexas.edu>
2018-11-26 21:48:33 -0600

    Cleaned up code

--- festival_scheduler.py
+++ festival_scheduler.py
@@ -1,34 +1,19 @@
 #Hard code variables for code development
-user_genre = ["Alternative", "Blues", "Christian", "Classical", "Country", "EDM", "Folk", "Hip Hop", "Indie", "Jazz", "Pop", "Rap", "Reggae", "Rock", "R&B"]
-user_locations = ["Austin", "New York", "Dallas", "San Francisco", "Santa Fe", "New Orleans"]
-user_times = [1, 13.5, 14, 22, 19.25, 20.5, 13.5, 27, 0, 24, -1.5, 22] #has 27, 0, 24, -1 to test that the times are greater than or equal to 0 and less than 24, and has duplicates to test conflicting times
+#user_genre = ["Alternative", "Blues", "Christian", "Classical", "Country", "EDM", "Folk", "Hip Hop", "Indie", "Jazz", "Pop", "Rap", "Reggae", "Rock", "R&B"]
+#user_locations = ["Austin", "New York", "Dallas", "San Francisco", "Santa Fe", "New Orleans"]
 
-
-#Fetches the genre from the user
-def get_location(user_location):
-	return user_location
-
-#Fetches the genre from the user
-def get_genre(user_genre):
-	return user_genre
 
 #Function that, given a location and genre, makes a list of possible festivals that the user would want to attend (including time conflicts, they will be accounted for in the scheduler)
 def select_festival(location, genre, global_festivals_list):
 	possible_festivals = []	
+	#Iterate through the global festivals list
 	for festival in global_festivals_list:
+		#check that the item in the list matches location and genre, add to possibility list
 		if (festival.location == location) and (genre in festival.genres):
 			possible_festivals.append(festival)
+	#Return error if there are no festivals
 	if len(possible_festivals) == 0:
 		return -1
+	#Return a list of all festivals in the desired location
 	else: 
 		return possible_festivals
-
-#can be commented out when the time comes to use it with GUI
-def main():
-
-location = get_location(user_locations[0])
-genre = get_genre(user_genre[0])
-
-possible_festivals = select_festival(location, genre)
-
-main()
