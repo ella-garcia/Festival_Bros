e928a03b6d18c2cbee49951db379aa3ab7d5c178
Garrett Odom <garrettodom97@Garretts-MacBook-Pro.local>
2018-12-02 19:50:45 -0600

    comments for Meera

--- GUI.py
+++ GUI.py
@@ -3,10 +3,16 @@ screen = 0
 city = ''
 festival = ''
 genre = ''
+
+#THESE ARE PLACEHOLDERS. THEY WILL ACTUALLY JUST BE WHATEVER CITIES/GENRES THEY DECIDED ON AND HAVE IN THEIR CODE
 cities = ["Austin", "New York", "Dallas", "San Francisco", "Santa Fe", "New Orleans", "New York"]
 genres = ["Alternative", "Blues", "Christian", "Classical", "Country", "EDM", "Folk", "Hip Hop", "Indie", "Jazz", "Pop", "Rap", "Rock", "R&B"]
 city_selected = -1
 genre_selected = -1
+band_selected = -1
+selected = 0
+bands = []
+showtimes = []
 frame = 0
 import sys
 
@@ -37,6 +43,10 @@ def draw():
 	global genres
 	global city_selected
 	global genre_selected
+	global band_selected
+	global selected
+	global bands
+	global showtimes
 	global frame
 	rectMode(CENTER)
 
@@ -74,10 +84,16 @@ def draw():
 
 		if (mousePressed and mouseX > 15 and mouseX < 285 and mouseY > 265 and mouseY < 365):
 			screen = 1
+			frame = 0
+
 		if (mousePressed and mouseX > 315 and mouseX < 585 and mouseY > 265 and mouseY < 365):
 			screen = 2
+			frame = 0
+
 		if (mousePressed and mouseX > 615 and mouseX < 885 and mouseY > 265 and mouseY < 365):
 			screen = 3
+			frame = 0
+
 		if (mousePressed and mouseX > 850 and mouseX < 900 and mouseY > 0 and mouseY < 50):
 			sys.exit()
 
@@ -109,6 +125,8 @@ def draw():
 			text(cities[i],150,offset+160)
 			if(mousePressed and mouseX > 75 and mouseX < 225 and mouseY > offset+125 and mouseY < offset+175):
 				city_selected = i
+
+				#THIS IS THE CITY THE USER HAS SELECTED TO GET A FESTIVAL SUGGESTION FROM
 				city = cities[i]
 			offset += 75
 
@@ -126,6 +144,8 @@ def draw():
 			text(genres[i],350+shift,offset+160)
 			if(mousePressed and mouseX > 275+shift and mouseX < 425+shift and mouseY > offset+125 and mouseY < offset+175):
 				genre_selected = i
+
+				#THIS IS THE GENRE THE USER HAS SELECTED TO GET A FESTIVAL SUGGESTION FROM
 				genre = genres[i]
 			offset += 75
 			if(i == 6):
@@ -138,12 +158,16 @@ def draw():
 		fill(0)
 		text("Submit",750,135)
 		if(mousePressed and mouseX > 675 and mouseX < 825 and mouseY > 100 and mouseY < 175 and city_selected != -1 and genre_selected != -1):
+
+			#THIS NEEDS TO BE THE SUGGESTED FESTIVAL FROM THE ALGORITHM
 			festival = "~Festival Name Here~"
 
 		# display the suggested festival
 		text(festival,750,300)
 
 		if(city_selected != -1 and genre_selected != -1 and mousePressed and mouseX > 625 and mouseX < 875 and mouseY > 275 and mouseY < 325 and frame>5):
+
+			#THIS NEED TO BE THE LINK TO THAT FESTIVALS WEBSITE (MAY NOT HAVE THIS YET)
 			link("https://github.com/ella-garcia/Festival_Bros")
 			frame = 0
 
@@ -153,32 +177,124 @@ def draw():
 
 		# read button press based on mouse location
 
-		if (mousePressed and mouseX > 800 and mouseX < 850 and mouseY > 15 and mouseY < 65):
+		if (mousePressed and mouseX > 765 and mouseX < 885 and mouseY > 15 and mouseY < 65):
 			screen = 0
 
 	# Scheduler Page
 
 	if (screen == 2):
 
-		# text and button outlines
-
 		background(200)
 		textFont(title_font)
 		fill(0)
 		textAlign(CENTER)
-		text("Festival Scheduler", 450, 50)
+		text("Festivals Near Me", 450, 50)
 
-		fill(50,200,50)
-		rect(825,40,120,25)
+		if (selected == 0):
 
-		fill(0)
-		textFont(detail_font)
-		text("Main Menu",825,50)
+			# loop to create city buttons and determine which one is currently selected
+			offset = 0
+			for i in range(0,7):
+				fill(200,50,50)
+				rect(350,offset+150,150,50)
+				fill(0)
+				textFont(detail_font)
+				text(cities[i],350,offset+160)
+				if(mousePressed and mouseX > 275 and mouseX < 425 and mouseY > offset+125 and mouseY < offset+175 and frame > 5):
 
-		# read button press based on mouse location
+					#THIS IS THE CITY THE USER WANTS TO SEE ALL FESTIVALS FOR TO THEN CHOOSE WHICH ONE TO MAKE A SCHEDULE FOR
+					city2 = cities[i]
+					selected = 1
+					bands = []
+					showtimes = []
+				offset += 75
 
-		if (mousePressed and mouseX > 800 and mouseX < 850 and mouseY > 15 and mouseY < 65):
-			screen = 0
+			# text and button outlines
+
+			fill(50,200,50)
+			rect(825,40,120,25)
+
+			fill(0)
+			textFont(detail_font)
+			text("Main Menu",825,50)
+
+			# read button press based on mouse location
+
+			if (mousePressed and mouseX > 765 and mouseX < 885 and mouseY > 15 and mouseY < 65):
+				screen = 0
+
+		if (selected == 1):
+
+			#THESE NEED TO BE THE ARTISTS AND TIMES FOR THE SELECTED FESTIVAL
+			artists = ['aaaaaaa','bbbbbbb','ccccccc','ddddddd','eeeeeeee','fffffffff']
+			times = ['12:00','12:30','1:30','4:00','5:30','7:00','8:00']
+
+			# loop to create artist buttons and determine which one is currently selected
+			offset = 0
+			for i in range(0,len(artists)):
+				fill(200,50,50)
+				rect(150,offset+150,150,50)
+				fill(0)
+				textFont(detail_font)
+				text(artists[i],150,offset+150)
+				text(times[i],150,offset+170)
+				if(mousePressed and mouseX > 75 and mouseX < 225 and mouseY > offset+125 and mouseY < offset+175):
+					band_selected = i
+					enter_band = artists[i]
+					enter_time = times[i]
+					if enter_band not in bands:
+						bands.append(enter_band)
+						showtimes.append(enter_time)
+				offset += 75
+
+			textFont(button_font)
+			text("My Schedule",650,100)
+			line(400,120,800,120)
+
+			offset2 = 0
+			for i in range(0,len(bands)):
+				textFont(detail_font)
+				text(bands[i],450,offset2+150)
+				text(showtimes[i],720,offset2+150)
+				line(400,offset2+170,800,offset2+170)
+				offset2+=50
+
+			fill(0,75,200)
+			rect(700,600,120,50)
+
+			fill(0)
+			textFont(detail_font)
+			text("Reset",700,610)
+
+			if (mousePressed and mouseX > 640 and mouseX < 760 and mouseY > 575 and mouseY < 625):
+				bands = []
+				showtimes = []
+
+			fill(50,200,50)
+			rect(500,600,120,50)
+
+			fill(0)
+			textFont(detail_font)
+			text("Save",500,610)
+
+			if (mousePressed and mouseX > 440 and mouseX < 560 and mouseY > 575 and mouseY < 625):
+
+				#THIS IS WHEN THE SAVE BUTTON IS PRESSED. NEED TO SEND VARIABLES "BANDS" AND "SHOWTIMES" (SCHEDULE USER HAS MADE)
+				#BACK TO ALGORITHM TO BE PRINTED ON A TEXT FILE
+				a=1
+
+			fill(50,200,50)
+			rect(825,40,120,25)
+
+			fill(0)
+			textFont(detail_font)
+			text("Main Menu",825,50)
+
+			# read button press based on mouse location
+
+			if (mousePressed and mouseX > 765 and mouseX < 885 and mouseY > 15 and mouseY < 65):
+				screen = 0
+				selected = 0
 
 	# Randomizer Page
 
@@ -201,7 +317,7 @@ def draw():
 
 		# read button press based on mouse location
 
-		if (mousePressed and mouseX > 800 and mouseX < 850 and mouseY > 15 and mouseY < 65):
+		if (mousePressed and mouseX > 765 and mouseX < 885 and mouseY > 15 and mouseY < 65):
 			screen = 0
 
 	frame+=1
