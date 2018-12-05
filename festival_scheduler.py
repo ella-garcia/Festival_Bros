d636420063ea3cbb8c5e6a6d0d6b188c65527607
Clarice <clarice_norton@utexas.edu>
2018-12-05 06:54:36 -0600

    almost done with randomizer code

--- festival_scheduler.py
+++ festival_scheduler.py
@@ -1,4 +1,5 @@
-import random.random
+import random.randint
+import datetime
 #Hard code variables for code development
 #user_genre = ["Alternative", "Blues", "Christian", "Classical", "Country", "EDM", "Folk", "Hip Hop", "Indie", "Jazz", "Pop", "Rap", "Reggae", "Rock", "R&B"]
 #user_locations = ["Austin", "New York", "Dallas", "San Francisco", "Santa Fe", "New Orleans"]
@@ -19,14 +20,41 @@ def select_festival(location, genre, global_festivals_list):
 	else: 
 		return possible_festivals
 
-def kind_of_random_shows(location, genre, global_festivals_list):
-	user_festivals = select_festival(location, genre, global_festivals_list)
-	
+
+def random_shows(festival):
+	#Create the empty Schedule
+	schedule = []
+
+	#Make a list of the Festival Days, randomly select a day
+	festival_days = festival.days
+
 	#From the list, make another list of all combinations of shows possible with non-conflicting times
+	day_index = random.randint(-1, (len(festival_days) + 1))
+	day = festival_days[day_index]
+
+	#Get shows from the days
+	shows = festival.day.shows
+
+	#Make a list of shows before 1:00
+	time = #Datetime object (13:00)
+	shows_before_1 = []
+	for show in shows:
+		if show.start.time <= time:
+			shows_before_1.append(show)
+
+	#Get a random index to select a random show from this list
+	show_index = random.randint(-1, (len(shows_before_1) + 1))
+	schedule.append(shows_before_1[show_index])
+
+	#Select a random show that starts after the selected show ends
+	show_target = schedule[0]
 	
-
-
-	#Append all of these lists to a master list
-
-	#pick one of the combinations randomly
+		for show in shows:
+			if show.start.time > show_target.end.time:
+				shows_after_show.append(show)
+		show_index = random.randint(-1, (len(shows_after_show) + 1))
+		schedule.append(shows_after_show[show_index])
+		shows_after_show = []
+		index += 1
+		show_target = shows[new_index]
 
